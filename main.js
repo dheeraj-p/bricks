71ed53a135c6113685c108bf09634a2160880e92
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-01-12 11:30:21 +0530

    Generate paddle from javascript

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+const toPixels = function(value) {
+  return `${value}px`;
+};
+
+const getPaddleView = function(document) {
+  return document.getElementById('paddle_1');
+};
+
+const getGameWindow = function(document) {
+  return document.getElementById('game-window');
+};
+
+const drawPaddle = function(document, paddle) {
+  const paddleView = getPaddleView(document);
+  paddleView.style.width = toPixels(paddle.getWidth());
+  paddleView.style.height = toPixels(paddle.getHeight());
+  paddleView.style.bottom = toPixels(paddle.getBottom());
+  paddleView.style.left = toPixels(paddle.getLeft());
+};
+
 const createPaddle = function(document) {
-  const gameWindow = document.getElementById('game-window');
+  const gameWindow = getGameWindow(document);
   const paddleView = document.createElement('div');
+  const paddle = new Paddle(20, 100, 10, 430);
   paddleView.className = 'paddle';
   paddleView.id = 'paddle_1';
   gameWindow.appendChild(paddleView);
+  drawPaddle(document, paddle);
 };
 
 const intializeGame = function() {
---
+++ paddle.js
@@ -0,0 +1,24 @@
+class Paddle {
+  constructor(height, width, bottom, left) {
+    this.height = height;
+    this.width = width;
+    this.bottom = bottom;
+    this.left = left;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getBottom() {
+    return this.bottom;
+  }
+
+  getLeft() {
+    return this.left;
+  }
+}
