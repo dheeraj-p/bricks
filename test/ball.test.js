13a35927c8d6c3945f7d4263c7c9ed8c4e6b705b
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-01-13 18:00:15 +0530

    Fix test for paddle and ball

--- test/ball.test.js
+++ test/ball.test.js
@@ -1,15 +1,15 @@
 describe('Ball', function() {
   const expect = chai.expect;
   const velocity = new Velocity(10, 90);
-  const ball = new Ball(30, 10, 430, velocity);
+  const ball = new Ball(30, 430, 10, velocity);
   it('move: should move ball to a given position for a specific velocity', function() {
     ball.move();
     expect(ball)
-      .to.have.property('bottom')
+      .to.have.property('y')
       .to.equal(20);
 
     expect(ball)
-      .to.have.property('left')
+      .to.have.property('x')
       .to.equal(430);
   });
 });
--- test/paddle.test.js
+++ test/paddle.test.js
@@ -2,20 +2,20 @@ describe('Paddle', function() {
   const expect = chai.expect;
   let paddle;
   beforeEach(function() {
-    paddle = new Paddle(20, 100, 10, 430);
+    paddle = new Paddle(20, 100, 430, 10);
   });
 
   it('moveLeft: should move paddle to left with speed 10', function() {
     paddle.moveLeft();
     expect(paddle)
-      .to.have.property('left')
+      .to.have.property('x')
       .equal(420);
   });
 
   it('moveRight: should move paddle to right with speed 10', function() {
     paddle.moveRight();
     expect(paddle)
-      .to.have.property('left')
+      .to.have.property('x')
       .equal(440);
   });
 });
